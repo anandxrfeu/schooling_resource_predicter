7bc2ef54cdd6bb2ab1aae97efc02760732aa3638
Anand Tamilchelvan <anandxrfeu@gmail.com>
2023-09-08 14:57:42 -0300

    Predict Funding V2

--- api/fast.py
+++ api/fast.py
@@ -14,9 +14,11 @@ from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
-"""
- FOR ML
-"""
+def load_saved_model():
+    folder_path = "/models/"
+    file_name = "model.h5"
+    file_path = os.path.join(os.getcwd()+folder_path, file_name)
+    return load_model(file_path)
 
 def ml_preproc():
     folder_path = "/raw_data/"
@@ -57,7 +59,8 @@ def ml_preproc():
 
     return y_train_urban
 
-y_train_urban = ml_preproc()
+app.state.model = load_saved_model()
+app.state.y_train_urban = ml_preproc()
 
 
 # Load .env file
@@ -117,6 +120,7 @@ async def predict_funding(ibge_code, year: int = 2023, localization: str = 'Urba
 @app.get("/predict-funding/v2/{ibge_code}")
 async def new_end_point(ibge_code, year: int = 2023, localization: str = 'Urbana', passing_rate: float = 90):
 
+    # to be removed
     localization = 'Urbana'
 
     # Convert IBGE code to integer
@@ -127,13 +131,23 @@ async def new_end_point(ibge_code, year: int = 2023, localization: str = 'Urbana
     file_name = "all_expanded_ML.csv"
     file_path = os.path.join(os.getcwd()+folder_path, file_name)
     municipality_data = pd.read_csv(file_path)
+    #print("municipality_data b4>> ", municipality_data.head(3))
+
     municipality_data = municipality_data[(municipality_data['Código_IBGE'] == ibge_code) &  (municipality_data['Localização'] == localization)].sort_values(by='Ano', ascending=False)
 
+    #print("municipality_data afr >> ", municipality_data.head(3))
 
     if municipality_data.empty:
         raise HTTPException(status_code=404, detail="Municipality with this configuration does not exist")
 
-    municipality_ML = municipality_data[['Ano', 'Código_IBGE', 'Aprovação', 'Reprovação', 'Abandono',
+
+
+
+
+    ## Make prediction ##
+
+    # # Prepare data
+    municipality_ML = municipality_data.copy()[['Ano', 'Código_IBGE', 'Aprovação', 'Reprovação', 'Abandono',
        'Matrículas', 'Docentes', 'Estabelecimentos', 'Turmas', 'PIB',
        'Poverty_%', 'Unemployed_%', 'Acesso_a_internet_%',
        'Adjusted_population', 'Adjusted_funding']]
@@ -144,49 +158,56 @@ async def new_end_point(ibge_code, year: int = 2023, localization: str = 'Urbana
     feature_scaler = MinMaxScaler(feature_range=(0, 1))
     sample_features_scaled = feature_scaler.fit_transform(municipality_ML.drop(columns=['Ano', 'Código_IBGE', 'Adjusted_funding']))
 
+    # Load model
+    new_model = app.state.model
 
-
-    # Load the pre-trained model
-    folder_path = "/models/"
-    file_name = "model.h5"
-    file_path = os.path.join(os.getcwd()+folder_path, file_name)
-    new_model = load_model(file_path)
-
+    # Get preprocessed target
+    y_train_urban = app.state.y_train_urban
 
     # Make predictions using the model
     predictions_scaled = new_model.predict(sample_features_scaled[-3:,:].reshape(1, 3, -1))
 
     # Inverse transform the scaled predictions to the original scale
     target_scaler = MinMaxScaler().fit(y_train_urban)  # Assuming y_train_urban is your training target data
-    predictions_original_scale = target_scaler.inverse_transform(predictions_scaled)
+
     # The predictions for the next three years are now stored in predictions_original_scale
+    predictions_original_scale = target_scaler.inverse_transform(predictions_scaled)
 
-    municipality_ML = municipality_ML.to_dict('records')
-
-
-
-    # # Get the most recent record for the municipality
-    municipality = municipality_data.head(0)
-    # last_year = int(municipality['Ano'])
-
-    # # Extract the funding values from the past for the same municipality and localization
-    #funding = municipality_data[municipality_data['Codigo_IBGE'] == ibge_code].sort_values(by='Ano', ascending=False)[['Ano', 'adjusted_funding']].to_dict('records')
-    #print(funding)
-    # adjusted_funding_values = [item['adjusted_funding'] for item in funding]
+    ## Combine Prediction data with Original data ##
 
 
+    # Create a dictionary with the first row of the dataset, using column names as keys
+    municipality_data_2020 = municipality_data.iloc[0].to_dict()
 
-    # # Make multiple prediction for all years from current to selected years
-    # if year - last_year > 0:
-    #     for yr in range(last_year+1, year+1,):
-    #         funding.append({'Ano':yr, 'adjusted_funding' : random.choice(adjusted_funding_values)})
+    # Create a dictionary with 'Ano' (Year) as the key and 'Adjusted_funding' as the value
+    adjusted_funding_dict = dict(zip(municipality_data['Ano'], municipality_data['Adjusted_funding']))
 
-    # funding = sorted(funding, key=lambda x: x['Ano'])
-    # # Update the municipality dictionary with the newly generated funding data
-    # municipality['Adjusted_funding'] = funding
-    # municipality['Historic_funding'] = municipality.pop('Adjusted_funding')
+    # Given list of lists with funding information for years starting from 2021 and onwards
+    #funding_list_of_lists = [[12102227.0, 13575426.0, 13404874.0]]
+    funding_list_of_lists = predictions_original_scale.flatten().tolist()
 
-    municipality = {}
-    municipality["data"] = municipality_ML
+    # Generate years for the new funding information, starting from 2021
+    new_funding_years = list(range(2021, 2021 + len(funding_list_of_lists)))
 
-    return municipality
+    # Create a new dictionary with the new funding information
+    new_funding_dict = dict(zip(new_funding_years, funding_list_of_lists))
+
+    # Update the 'adjusted_funding_dict' with the new funding information
+    adjusted_funding_dict.update(new_funding_dict)
+
+    # Add the 'adjusted_funding_dict' as a value in the 'first_row_dict' under the key 'Historical_funding'
+    municipality_data_2020['Historic_funding'] = adjusted_funding_dict
+
+    # Transform the dictionary into a list of dictionaries with 'Ano' and 'Adjusted_funding' keys
+    transformed_historic_funding = [{"Ano": year, "Adjusted_funding": funding} for year, funding in municipality_data_2020['Historic_funding'].items()]
+    # Sort the transformed list of dictionaries by the 'Ano' key
+    sorted_transformed_historic_funding = sorted(transformed_historic_funding, key=lambda x: x['Ano'])
+    municipality_data_2020['Historic_funding'] = sorted_transformed_historic_funding
+
+    # Display the transformed 'Historic_funding'
+    transformed_historic_funding
+
+    # Delete the
+    del municipality_data_2020["Adjusted_funding"]
+
+    return municipality_data_2020
